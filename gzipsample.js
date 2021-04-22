efabc50d3e0d49094f541051361a374a1e44997d
nitin.eusebius <nitin.eusebius@gmail.com>
2021-04-22 00:52:58 -0700

    adding gzipsample.js

---
+++ gzipsample.js
@@ -0,0 +1,35 @@
+// Node.js program to demonstrate the	
+// gzip() method
+
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('logs_sample.json');
+let logsObj = JSON.parse(rawdata);
+console.log(logsObj);
+
+
+var myJSON = JSON.stringify(logsObj);
+console.log(myJSON)
+
+// Including zlib module
+const zlib = require("zlib");
+
+// Declaring input and assigning
+// it a value string
+
+var input = myJSON
+
+// Calling gzip method
+zlib.gzip(input, (err, buffer) => {
+
+if (!err) {
+
+	console.log(buffer.toString('base64'));
+}
+else {
+	console.log(err);
+}
+});
+console.log("Data Compressed...");
+
